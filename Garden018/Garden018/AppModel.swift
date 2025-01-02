c49500d5d9dad3346aa53c2a8c43277433ca6cd3
Joseph Simpson <joe@radicalappdev.com>
2025-01-02 05:31:30 -0500

    Starting garden 018

---
+++ Garden018/Garden018/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Garden018
+//
+//  Created by Joseph Simpson on 1/2/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ Garden018/Garden018/Garden018App.swift
@@ -0,0 +1,33 @@
+//
+//  Garden018App.swift
+//  Garden018
+//
+//  Created by Joseph Simpson on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Garden018App: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
