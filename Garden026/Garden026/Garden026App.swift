d8e454aac4dc994e966b983a50eb9df29f0d88ba
Joseph Simpson <joe@radicalappdev.com>
2025-05-15 12:36:23 -0400

    Starting garden 026

---
+++ Garden026/Garden026/Garden026App.swift
@@ -0,0 +1,33 @@
+//
+//  Garden026App.swift
+//  Garden026
+//
+//  Created by Joseph Simpson on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Garden026App: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
