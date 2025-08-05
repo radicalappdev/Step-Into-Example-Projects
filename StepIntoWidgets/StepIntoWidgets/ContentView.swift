7f1e6fc0afcdd345b4397387afc11f69c0d14686
Joseph Simpson <joe@radicalappdev.com>
2025-08-05 08:28:06 -0400

    Adding some UI to the content view for the next widget

--- StepIntoWidgets/StepIntoWidgets/ContentView.swift
+++ StepIntoWidgets/StepIntoWidgets/ContentView.swift
@@ -10,17 +10,51 @@ import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
+    @State private var currentEmoji: String = ""
 
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
-
-            ToggleImmersiveSpaceButton()
+            Text("\(currentEmoji)")
+                .font(.extraLargeTitle2)
+                .padding(.bottom)
+            
+            HStack(spacing: 20) {
+                Button(action: {
+                    saveEmojiToUserDefaults("🚀")
+                }) {
+                    Text("🚀")
+                }
+                
+                Button(action: {
+                    saveEmojiToUserDefaults("🌸")
+                }) {
+                    Text("🌸")
+                }
+                
+                Button(action: {
+                    saveEmojiToUserDefaults("🐸")
+                }) {
+                    Text("🐸")
+                }
+            }
+            .controlSize(.extraLarge)
         }
         .padding()
+        .onAppear {
+            loadCurrentEmoji()
+        }
+    }
+    
+    private func saveEmojiToUserDefaults(_ emoji: String) {
+        UserDefaults.standard.set(emoji, forKey: "dataEmojiExample")
+        print("Saved emoji: \(emoji) to UserDefaults")
+        currentEmoji = emoji
+    }
+    
+    private func loadCurrentEmoji() {
+        if let savedEmoji = UserDefaults.standard.string(forKey: "dataEmojiExample") {
+            currentEmoji = savedEmoji
+        }
     }
 }
 
