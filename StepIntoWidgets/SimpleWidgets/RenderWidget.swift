a533b852d4037821a4b74defd778de9be4000094
Joseph Simpson <joe@radicalappdev.com>
2025-08-06 10:01:54 -0400

    Adding an app intent to switch modes

--- StepIntoWidgets/SimpleWidgets/RenderWidget.swift
+++ StepIntoWidgets/SimpleWidgets/RenderWidget.swift
@@ -11,14 +11,14 @@ import AppIntents
 
 struct RenderProvider: AppIntentTimelineProvider {
     func placeholder(in context: Context) -> RenderEntry {
-        RenderEntry(date: Date(), configuration: ConfigurationAppIntent())
+        RenderEntry(date: Date(), configuration: RenderConfigurationAppIntent())
     }
 
-    func snapshot(for configuration: ConfigurationAppIntent, in context: Context) async -> RenderEntry {
+    func snapshot(for configuration: RenderConfigurationAppIntent, in context: Context) async -> RenderEntry {
         RenderEntry(date: Date(), configuration: configuration)
     }
 
-    func timeline(for configuration: ConfigurationAppIntent, in context: Context) async -> Timeline<RenderEntry> {
+    func timeline(for configuration: RenderConfigurationAppIntent, in context: Context) async -> Timeline<RenderEntry> {
         var entries: [RenderEntry] = []
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
@@ -39,55 +39,49 @@ struct RenderProvider: AppIntentTimelineProvider {
 
 struct RenderEntry: TimelineEntry {
     let date: Date
-    let configuration: ConfigurationAppIntent
+    let configuration: RenderConfigurationAppIntent
 }
 
 struct RenderWidgetsEntryView : View {
     var entry: RenderProvider.Entry
     @Environment(\.widgetRenderingMode) var renderingMode
 
-    var body: some View {
-        if renderingMode == .fullColor {
-            VStack() {
-                Label("WANTED", systemImage: "target")
-                    .font(.headline)
-
-                Image(.jsWidget)
-                    .resizable()
-                    .scaledToFit()
-
-                Text("For taking silly photos at a metro park")
-                    .font(.caption)
-
-            }
-        } else if renderingMode == .accented {
-            VStack() {
-                Label("WANTED", systemImage: "target")
-                    .font(.headline)
-
-                Image(.jsWidget)
-                    .resizable()
-                    .widgetAccentedRenderingMode(.desaturated)
-                    .scaledToFit()
-
-                Text("For taking silly photos at a metro park")
-                    .font(.caption)
-                    .widgetAccentable()
-                    .multilineTextAlignment(.center)
-
-            }
+    var widgetRenderingMode: WidgetAccentedRenderingMode {
+        switch entry.configuration.value {
+        case .accented:
+            return .accented
+        case .accentedDesaturated:
+            return .accentedDesaturated
+        case .desaturated:
+            return .desaturated
+        case .fullColor:
+            return .fullColor
         }
     }
 
+    var body: some View {
+        VStack() {
+            Label("WANTED", systemImage: "target")
+                .font(.headline)
 
+            Image(.jsWidget)
+                .resizable()
+                .widgetAccentedRenderingMode(widgetRenderingMode)
+                .scaledToFit()
+
+            Text("For taking silly photos at a metro park")
+                .font(.caption)
+                .widgetAccentable()
+                .multilineTextAlignment(.center)
+        }
+    }
 }
-
 
 struct RenderWidgets: Widget {
     let kind: String = "RenderWidgets"
 
     var body: some WidgetConfiguration {
-        AppIntentConfiguration(kind: kind, intent: ConfigurationAppIntent.self, provider: RenderProvider()) { entry in
+        AppIntentConfiguration(kind: kind, intent: RenderConfigurationAppIntent.self, provider: RenderProvider()) { entry in
             RenderWidgetsEntryView(entry: entry)
                 .containerBackground(.white.gradient, for: .widget)
         }
