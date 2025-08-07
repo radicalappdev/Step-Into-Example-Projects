e2f95e50cc5210369e4e1aca464d675a1e8d7d07
Joseph Simpson <joe@radicalappdev.com>
2025-08-07 18:53:17 -0400

    CYA

--- ResourceLibrarySnapshop/ResourceLibrarySnapshop/ImmersiveView.swift
+++ ResourceLibrarySnapshop/ResourceLibrarySnapshop/ImmersiveView.swift
@@ -12,7 +12,7 @@ import WebKit
 
 struct AttachmentView: View {
 
-    var url = "https://github.com/Dave-Ed-Cast/SetMode?tab=readme-ov-file"
+    var url = "https://github.com/XanderXu/RealityShaderExtension"
 
     var body: some View {
         VStack {
